398aaf1c2f178c058f423934e43cb0fe06afef08
Anna <giannagrale@gmail.com>
2023-01-29 20:16:35 +0300

    Add Commit Hook

--- post-commit.sh
+++ post-commit.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 #
-curl -X POST "http://jenkins:8080/git/notifyCommit?url=git@gitserver:tools/common.git
-"
+curl -X POST "http://jenkins:8080/git/notifyCommit?url=git@gitserver:tools/common.git"
