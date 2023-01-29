c58fc0147f37c44f06b55085ad57c80fcbb9798a
Anna <giannagrale@gmail.com>
2023-01-29 21:09:54 +0300

    Add Commit Hook

--- post-commit.sh
+++ post-commit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl --user hanna:11e74fb15c9e33694f9cd4cba974026a96 http://localhost:8080/job/gitbuild/build?token=hannadocker1234
+curl --user "hanna:11e74fb15c9e33694f9cd4cba974026a96 http://localhost:8080/job/gitbuild/build?token=hannadocker1234"
 
